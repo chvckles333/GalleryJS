d3aa4b9473941a54361f326fd86c907e517f9b9e
Unknown <cfranklin8@mail.csuchico.edu>
2017-04-25 16:56:39 -0700

    GalleyJS Lab Part 1

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
-
+var chico = ["Big Chico Creek","Bidwell Mansion","George Peterson Rose Garden",
+            "Old Hutch's Plaza","Chico City Plaza"];
+var current;
 
 function shuffleGallery()
 {
-
+  var index = Math.floor(Math.random() * chico.length);
+  while(index == current) {
+    index = Math.floor(Math.random() * chico.length);
+  }
+  console.log(chico[index]);
+  current = index;
 }
